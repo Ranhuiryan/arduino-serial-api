ab401b727fa19c6df064ef2e0e2def87a11d4be7
Ranhuiryan <qdgjhrh@outlook.com>
2024-07-12 19:55:51 +0800

    refactor: Convert distance and speed units to pulses for sending data to Arduino

--- send_data.py
+++ send_data.py
@@ -5,11 +5,17 @@ from flask import Flask, request, jsonify
 # 初始化串口
 arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=.1)
 
-def write_to_arduino(data):
-    arduino.write(bytes(data, 'utf-8'))
-    time.sleep(0.05)
-    response = arduino.readline()
-    return response.decode('utf-8')
+# def write_to_arduino(data):
+#     arduino.write(bytes(data, 'utf-8'))
+#     time.sleep(0.05)
+#     response = arduino.readline()
+#     return response.decode('utf-8')
+
+def convert_distance_to_pulse(distance):
+    return distance / 0.5 * 800   # 0.5mm导程对应800个脉冲
+
+def convert_pulse_to_distance(pulse):
+    return pulse / 800 * 0.5
 
 # 初始化Flask应用
 app = Flask(__name__)
@@ -18,14 +24,25 @@ app = Flask(__name__)
 def send_data():
     if request.is_json:
         req = request.get_json()
-        speed = req.get("speed")
+        speed = req.get("speed")    # 速度单位：mm/s
         acceleration = req.get("acceleration")
-        distance = req.get("distance")
+        distance = req.get("distance")     # 距离单位：mm
         
         if speed is not None and acceleration is not None and distance is not None:
-            data_to_send = f"{speed}, {acceleration}, {distance}"
-            response = write_to_arduino(data_to_send)
-            return jsonify({"status": "success", "response": data_to_send}), 200
+            speed = convert_distance_to_pulse(speed)
+            pulse = convert_distance_to_pulse(distance)
+            data_to_send = f"{speed}, {acceleration}, {pulse}"
+            # current_position = -999999
+            arduino.write(bytes(data_to_send, 'utf-8'))
+            time.sleep(0.05)
+            # while int(arduino.readline().decode('utf-8').split(",")[0]) != current_position:
+            #     response = arduino.readline().decode('utf-8')
+            #     current_position = int(response.split(",")[0])
+            #     current_speed = int(response.split(",")[1])
+            #     time.sleep(1.05)
+
+            # return jsonify({"status": "success", "position": current_position, "speed": current_speed}), 200
+            return jsonify({"status": "success"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data"}), 400
     else:
